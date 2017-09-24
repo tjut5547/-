719eadfe9e2a78cc6e10fd14babe768d836b2d9a
tjut5547 <1021129753@qq.com>
2017-09-24 18:14:09 +0800

    修改一些bug 增加一部分注释

--- src/DecisionTree.py
+++ src/DecisionTree.py
@@ -1,14 +1,12 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from PreProcess import *
 
-vectorizer = TfidfVectorizer(max_df=0.5, max_features=5000, min_df=2, use_idf=True, lowercase=False,
-                             decode_error='ignore', analyzer=str.split)
-
-
-def DecisionTreeClassify(normal, spam):
-    data = Data(normal, spam)
+def DecisionTreeClassifyTfidf(normal, spam, stop):
+    data = Data(normal, spam, stop)
+    # 统计每个词的TF-IDF
     vectorizer = TfidfVectorizer(max_df=0.5,
                                  max_features=5000,
                                  min_df=2,
@@ -25,19 +23,56 @@ def DecisionTreeClassify(normal, spam):
                                           max_depth=None,
                                           min_samples_split=2,
                                           min_samples_leaf=2)
+    # 训练加上测评
     DecisionTree.fit(train_x, train_y)
     print(DecisionTree.score(test_x, test_y))
 
+    # 特征提取，提取词汇
     words = vectorizer.get_feature_names()
     feature_importance = DecisionTree.feature_importances_
     word_importances_dict = dict(zip(words, feature_importance))
-    word_importances_dict = dict(sorted(word_importances_dict.items(), key=lambda val:val[1], reverse=True))
 
     number = 0
-    for item in word_importances_dict:
+    for word, importance in sorted(word_importances_dict.items(), key=lambda val:val[1], reverse=True):
+        print(word, importance)
         number += 1
-        print(item, end=" ")
         if number == 200:
             break
 
-DecisionTreeClassify('../data/normal.txt', '../data/spam.txt')
+
+def DecisionTreeClassifyTf(normal, spam, stop):
+    data = Data(normal, spam, stop)
+    # 统计每个词的TF-IDF
+    vectorizer = CountVectorizer(max_df=0.6,
+                                 max_features=5000,
+                                 min_df=2,
+                                 decode_error='ignore',
+                                 analyzer=str.split).fit(data.data)
+    train_x, test_x, train_y, test_y = train_test_split(data.data, data.label, test_size=0.1)
+    train_x = vectorizer.transform(train_x)
+    test_x = vectorizer.transform(test_x)
+
+    DecisionTree = DecisionTreeClassifier(criterion="entropy",
+                                          splitter="best",
+                                          max_depth=None,
+                                          min_samples_split=2,
+                                          min_samples_leaf=2)
+    # 训练加上测评
+    DecisionTree.fit(train_x, train_y)
+    print(DecisionTree.score(test_x, test_y))
+
+    # 特征提取，提取词汇
+    words = vectorizer.get_feature_names()
+    feature_importance = DecisionTree.feature_importances_
+    word_importances_dict = dict(zip(words, feature_importance))
+
+    number = 0
+    for word, importance in sorted(word_importances_dict.items(), key=lambda val:val[1], reverse=True):
+        print(word, importance)
+        number += 1
+        if number == 200:
+            break
+
+
+# DecisionTreeClassifyTfidf('../data/normal.txt', '../data/spam.txt', "../data/stopword.txt")
+DecisionTreeClassifyTf('../data/normal.txt', '../data/spam.txt', "../data/stopword.txt")
--- src/PreProcess.py
+++ src/PreProcess.py
@@ -1,21 +1,43 @@
 import jieba
 
+class stopword(object):
+    def __init__(self, file):
+        fd = open(file, encoding='utf-8')
+        self.data = []
+        for word in fd:
+            self.data.append(word.strip())
+
+
 class Data(object):
-    def __init__(self, normal, spam):
-        normal_file = open(normal, "r", encoding='utf-8')
-        spam_file = open(spam, "r", encoding='utf-8')
+    def __init__(self, normal, spam, stop):
+        # 去停用词
+        def remove_stopwords(stop_list, data_list):
+            result = []
+            # 这里面的leave_word不能作为最终的特征，因为leave_word当中每个词只出现一次
+            # 当多个非停用词出现在一句话中的时候，信息就会损失
+            leave_word = set(data_list) - set(stop_list)
+            for word in data_list:
+                if word in leave_word:
+                    result.append(word)
+            return result
 
         self.data = []
         self.label = []
+        normal_file = open(normal, "r", encoding='utf-8')
+        spam_file = open(spam, "r", encoding='utf-8')
+        self.stopwords = set(stopword(stop).data)
+
         for line in normal_file:
             _, content = line.split('::')
-            content = ' '.join(list(jieba.cut(content)))
+            content = remove_stopwords(self.stopwords, list(jieba.cut(content)))
+            content = ' '.join(content)
             self.data.append(content)
             self.label.append(1)
 
         for line in spam_file:
             _, content = line.split('::')
-            # content = ' '.join(list(jieba.cut(content)))
-            content = ' '.join(list(jieba.cut(content)))
+            content = remove_stopwords(self.stopwords, list(jieba.cut(content)))
+            content = ' '.join(content)
             self.data.append(content)
             self.label.append(0)
+
